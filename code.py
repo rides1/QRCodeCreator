96673e59fb213dcda39b6a84ee422b52ca9ea709
rides1 <rpd35@njit.edu>
2024-01-27 21:46:27 -0500

    first commit

---
+++ code.py
@@ -0,0 +1,15 @@
+import image
+import qrcode
+
+qr = qrcode.QRCode(
+    version = 15,
+    box_size = 10,
+    border = 5
+)
+
+link = "https://www.google.com/search?sca_esv=602071010&rlz=1C1CHBF_enUS1057US1063&sxsrf=ACQVn0--gZshRV3WKut_wD6EO1krZEE0ig:1706407642391&q=funny+memes&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjakvfq__6DAxV8v4kEHeMVDj4Q0pQJegQIDBAB&biw=1536&bih=738&dpr=1.25#imgrc=ia9zCZwnqGjP6M"
+
+qr.add_data(link)
+qr.make(fit =True)
+img = qr.make_image(fill = 'black', back_color = "white")
+img.save("code.png")
