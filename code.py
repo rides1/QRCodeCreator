67e97944cfa6a3327b0e22d7769866ff251189e9
rides1 <rpd35@njit.edu>
2024-02-02 12:59:11 -0500

    final changes

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ qr = qrcode.QRCode(
     border = 5
 )
 
-link = "https://www.google.com/search?sca_esv=602071010&rlz=1C1CHBF_enUS1057US1063&sxsrf=ACQVn0--gZshRV3WKut_wD6EO1krZEE0ig:1706407642391&q=funny+memes&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjakvfq__6DAxV8v4kEHeMVDj4Q0pQJegQIDBAB&biw=1536&bih=738&dpr=1.25#imgrc=ia9zCZwnqGjP6M"
+link = input("Enter a link to create QR code ")
 
 qr.add_data(link)
 qr.make(fit =True)
